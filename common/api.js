fd4e8d3a7bf9464dbd082181f9f655c80af7041e
juerui <juerui@vip.qq.com>
2019-10-11 20:14:26 +0800

    首页-轮播图功能--请求真实接口-获取轮播图图片地址

---
+++ common/api.js
@@ -0,0 +1,8 @@
+const serverUrl = "https://www.imovietrailer.com/superhero";			// 生产环境
+// 权限QQ
+const qq = 'lee41289527';
+// const serverUrl = "https://www.imovietrailer-dev.com/superhero";		// 开发环境
+export default{	
+	serverUrl,
+	qq,
+}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,31 +2,65 @@
 	<view class="page">
 		<!-- 首页 -->
 		<swiper class="banner" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-			<swiper-item>
-				<image src="../../static/imgs/banner1.jpg" class="banner"></image>
-			</swiper-item>
-			<swiper-item >
-				<image src="../../static/imgs/banner2.jpg" class="banner"></image>
-			</swiper-item>
-			<swiper-item>
-				<image src="../../static/imgs/banner1.jpg" class="banner"></image>
-			</swiper-item>
+			<!-- 		
+				<swiper-item>
+					<image src="../../static/imgs/banner1.jpg" class="banner"></image>
+				</swiper-item>
+				<swiper-item >
+					<image src="../../static/imgs/banner2.jpg" class="banner"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../static/imgs/banner1.jpg" class="banner"></image>
+				</swiper-item> 
+			-->
+			<swiper-item v-for="(item,index) in bList" :key="index">
+				<image :src="item.image" class="banner"></image>
+			</swiper-item>>
 		</swiper>
 	</view>
 </template>
 
 <script>
+	// 引入Api配置文件
+	import api from "../../common/api.js";
+
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				// banner列表
+				bList: [],
 			}
 		},
 		onLoad() {
-
+			// 获取banner列表
+			this.getBanner()
 		},
 		methods: {
-
+			// 获取banner列表-接口
+			getBanner(){
+				const { serverUrl, qq } = api;
+				uni.request({
+				    url: serverUrl + '/index/carousel/list', //仅为示例，并非真实接口地址。
+					method: 'POST',
+				    data: {
+						qq: qq,
+				    },
+					header:{	
+						'content-type':'application/x-www-form-urlencoded',
+					},
+				    success: (res) => {
+						console.log(res.data);
+						const resData = res.data;
+						// 判断数据是否获取成功
+						if(resData.status === 200) {
+							this.bList = resData.data;
+						} else {
+							console.log(resData.msg)
+						}
+						
+				    }
+				});
+			}
 		}
 	}
 </script>
