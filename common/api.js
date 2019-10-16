d05354bcef8cafa84ab59efe8808fdf517ed0350
juerui <juerui@vip.qq.com>
2019-10-16 18:38:54 +0800

    电影详情页-基本布局1

--- common/api.js
+++ common/api.js
@@ -1,6 +1,6 @@
 const serverUrl = "https://www.imovietrailer.com/superhero";			// 生产环境
 // 权限QQ
-const qq = 'lee34708890';
+const qq = 'lee79914194';
 // const serverUrl = "https://www.imovietrailer-dev.com/superhero";		// 开发环境
 export default{	
 	serverUrl,
--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -1,6 +1,65 @@
 <template>
-	<view>
-		这是电影预告片的详情页
+	<view class="page">
+		<!-- 这是电影预告片的详情页 -->
+		<view class="g-hd">
+			<!-- 预告片 -->
+			<video
+			src="http://122.152.205.72:88/superhero/xman/DarkPhoenix/trailer.mp4" 
+			class="u-video"
+			controls></video>
+		</view>
+		<view class="g-bd">
+			<!-- 基本信息-start -->
+			<view class="m-info">
+				<view>
+					<image src="../../static/imgs/poster/justice.png" class="poster-img"></image>
+				</view>
+				<view>
+					<view class="info">
+						<view class="title">X战警：黑凤凰</view>
+						<view class="text">2018 / 美国 / 科幻 / 超级英雄</view>
+						<view class="text">原名：xman: Dark Phoenix</view>
+						<view class="text">影片时长：未知</view>
+						<view class="text">上映时间：2019-06-07(美国)</view>
+						<view class="score">
+							<view class="score-info">
+								<view class="score-wrapper">
+									<view class="">综合评分:</view>
+									<view class="">8.9:</view>	
+								</view>
+								<view class="zan-wrapper">
+									<view class="score-icon">
+										
+									</view>
+									<view class="num">
+										7892 人点赞
+									</view>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			<!-- 基本信息-end -->
+			
+			<!-- 剧情介绍-start -->
+			<view class="g-bd2">
+				
+			</view>
+			<!-- 剧情介绍-end -->
+			
+			<!-- 演职人员-start -->
+			<view class="g-bd3">
+				
+			</view>
+			<!-- 演职人员-end -->
+			
+			<!-- 剧照-start -->
+			<view class="g-bd4">
+				
+			</view>
+			<!-- 剧照-end -->
+		</view>
 	</view>
 </template>
 
@@ -23,6 +82,6 @@
 	}
 </script>
 
-<style>
-	@import ("./movie.scss");
+<style lang="scss">
+	@import './movie.scss'
 </style>
