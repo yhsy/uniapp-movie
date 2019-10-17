96301d305e74b6056b386b9db7f16f093c1e1667
juerui <juerui@vip.qq.com>
2019-10-17 17:54:37 +0800

    封面页-长按显示操作菜单功能(最多6个菜单)

--- pages/cover/cover.vue
+++ pages/cover/cover.vue
@@ -4,7 +4,12 @@
 		<view class="img-warpper">
 			<!-- <image src="../../static/imgs/poster/justice.png" class="img-cover" mode=""></image> -->
 			<!-- 图片模式:宽度不变，高度自适应 -->
-			<image :src="cover" class="img-cover" mode="widthFix"></image>
+			<image 
+				:src="cover" 
+				class="img-cover" 
+				mode="widthFix"
+				@longpress="touchOperation()"
+			></image>
 		</view>
 	</view>
 </template>
@@ -29,7 +34,21 @@
 			})
 		},
 		methods: {
-			
+			// 手指按住-操作菜单
+			touchOperation(){
+				// 显示操作菜单(下面弹出)
+				uni.showActionSheet({
+					// 菜单列表(最多6个)
+					itemList: ["下载图片","菜单2","菜单3"],
+					// 点击菜单事件
+					success:  (res) => {
+						// 获取菜单的索引
+						const tIndex = res.tapIndex + 1;
+						
+						console.log('选中了第' + (tIndex + 1) + '个按钮');
+					},
+				})
+			}
 		}
 	}
 </script>
