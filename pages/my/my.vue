147929864dfcedb264b08ba7b09f9474ad50d482
juerui <juerui@vip.qq.com>
2019-10-18 19:13:41 +0800

    我的页面-基础布局

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,22 @@
 <template>
-	<view>
-		我的页面
+	<view class="page-block g-box">
+		<!-- 我的页面 -->
+		<view class="g-hd">
+			<view class="m-hd">
+				<view class="">
+					<image src="../../static/imgs/icons/default-face.png"  class="img-avatar"></image>
+				</view>
+				<view class="">
+					<view class="info-wrap">
+						<view class="nickname">admin01</view>
+						<view class="username">账号:admin01</view>
+					</view>
+				</view>
+				<view class="">
+					<image src="../../static/imgs/icons/settings.png" class="icon-setting"></image>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +33,6 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	@import './my.scss'
 </style>
