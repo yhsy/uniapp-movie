fd0aa67ed11746165e2c098fb09c95137de9426a
juerui <juerui@vip.qq.com>
2019-10-18 19:14:15 +0800

    注册/登录页--创建

---
+++ pages/login/login.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		注册登录页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import './login.scss'
+</style>
