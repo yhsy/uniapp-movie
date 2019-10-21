2e9922f1f6b13243f5b7aefe6dce7378da6e31ac
juerui <juerui@vip.qq.com>
2019-10-21 14:39:52 +0800

    我的账号页-清理缓存和退出登录功能

--- pages/my/userInfo/userInfo.vue
+++ pages/my/userInfo/userInfo.vue
@@ -9,7 +9,8 @@
 						<view>
 							<view class="img-warp">
 								<view class="">
-									<image src="../../../static/imgs/icons/default-face.png" class="img-avatar" mode=""></image>
+									<!-- <image src="../../../static/imgs/icons/default-face.png" class="img-avatar" mode=""></image> -->
+									<image :src="info.faceImage" class="img-avatar" mode=""></image>
 								</view>
 								<view class="u-arrow">
 									<image src="../../../static/imgs/icons/left-gray-arrow.png" class="icon-arrow" mode=""></image>
@@ -25,7 +26,8 @@
 						<view>
 							<view class="img-warp">
 								<view class="text">
-									admin01
+									<!-- admin01 -->
+									{{info.nickname}}
 								</view>
 								<view class="u-arrow">
 									<image src="../../../static/imgs/icons/left-gray-arrow.png" class="icon-arrow" mode=""></image>
@@ -41,7 +43,8 @@
 						<view>
 							<view class="img-warp">
 								<view class="text">
-									1990-01-01
+									<!-- 1990-01-01 -->
+									{{info.birthday}}
 								</view>
 								<view class="u-arrow">
 									<image src="../../../static/imgs/icons/left-gray-arrow.png" class="icon-arrow" mode=""></image>
@@ -57,7 +60,8 @@
 						<view>
 							<view class="img-warp">
 								<view class="text">
-									男
+									<!-- 男 -->
+									{{info.sex===1 ? '男' : '女'}}
 								</view>
 								<view class="u-arrow">
 									<image src="../../../static/imgs/icons/left-gray-arrow.png" class="icon-arrow" mode=""></image>
@@ -71,10 +75,10 @@
 			
 		
 		<view class="m-ft">
-			<view class="item-menu">
+			<view class="item-menu" @click="clearStorage()">
 				清理缓存
 			</view>
-			<view class="item-menu">
+			<view class="item-menu" @click="loginOut()">
 				退出登录
 			</view>
 		</view>
@@ -84,10 +88,60 @@
 
 <script>
 	export default {
+		
+		onLoad() {
+			if(uni.getStorageSync('globalUser')){
+				// 获取登录的信息(本地Localstorege里)
+				const userInfo = uni.getStorageSync('globalUser');
+				
+				// 登录成功状态
+				if(userInfo){
+					this.info = userInfo;
+				}
+			} else {
+				// 跳转到登录页
+				uni.navigateTo({
+					url: '../../login/login'
+				})
+			}
+		},
 		data() {
 			return {
-				
+				info: {}
 			};
+		},
+		methods:{
+			// 清理缓存
+			clearStorage(){
+				// // 清理本地所有缓存
+				// uni.clearStorageSync();
+				// // 跳转到登录页
+				// uni.switchTab({
+				// 	url: '../../index/index'
+				// })
+				uni.showToast({
+					title: '清理缓存成功',
+					duration:2000,
+					icon: 'success'
+				})
+			},
+			// 退出登录
+			loginOut(){
+				uni.showLoading()
+				// 清理登录信息缓存
+				uni.removeStorageSync('globalUser');
+				// 跳转到首页
+				// uni.switchTab({
+				// 	url: '../../index/index'
+				// })
+				// 跳转到登录页
+				uni.navigateTo({
+					url: '../../login/login'
+				})
+				setTimeout(()=>{
+					uni.hideLoading()
+				},1000)
+			}
 		}
 	}
 </script>
