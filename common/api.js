68bb9820f6ee11fea0b9f43af5b0931d70365d96
juerui <juerui@vip.qq.com>
2019-10-21 19:38:25 +0800

    api接口更换

--- common/api.js
+++ common/api.js
@@ -1,6 +1,6 @@
 const serverUrl = "https://www.imovietrailer.com/superhero";			// 生产环境
 // 权限QQ
-const qq = 'lee60720041';
+const qq = 'lee96125779';
 // const serverUrl = "https://www.imovietrailer-dev.com/superhero";		// 开发环境
 export default{	
 	serverUrl,
