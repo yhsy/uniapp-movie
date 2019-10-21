edf057530963cf54b953c526a129cfde11fc7cbd
juerui <juerui@vip.qq.com>
2019-10-21 10:44:19 +0800

    登录页-基础布局

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,6 +1,35 @@
 <template>
-	<view>
-		注册登录页
+	<view class="page-block">
+<!-- 		<view class="g-bd">
+			<view class="avatar">
+				<image src="../../static/imgs/icons/default-face.png" class="img-avatar"></image>
+			</view>
+			<view class="username">
+				
+			</view>
+		</view> -->
+		<form @submit="" class="g-form">
+			<view class="m-form">
+				<view class="avatar-wrap">
+					<image src="../../static/imgs/icons/default-face.png" class="img-avatar"></image>
+				</view>
+				<view>
+					<view class="ipt-box">
+						<label>账号:</label>
+						<input type="text" value="" placeholder="请输入账号" class="ipt-username" />
+					</view>
+				</view>
+				<view class="ipt-pwd">
+					<view class="ipt-box">
+						<label>密码:</label>
+						<input type="password" password="true" value="" placeholder="请输入密码" class="ipt-username" />
+					</view>
+				</view>
+				<view class="ipt-btn">
+					<button type="primary" form-type="submit">注册/登录</button>
+				</view>
+			</view>
+		</form>
 	</view>
 </template>
 
