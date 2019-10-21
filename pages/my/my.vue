0a6728925316aebb7c6a3f6a5abdf7722def0038
juerui <juerui@vip.qq.com>
2019-10-21 10:10:37 +0800

    我的页面--布局完成(登录状态切换)

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -4,16 +4,22 @@
 		<view class="g-hd">
 			<view class="m-hd">
 				<view class="">
-					<image src="../../static/imgs/icons/default-face.png"  class="img-avatar"></image>
+					<image src="../../static/imgs/icons/default-face.png"  class="img-avatar" v-if="logins"></image>
+					<image src="http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_-5-AFyVyAABLIH8xBTw233.png" class="img-avatar" v-else></image>
 				</view>
 				<view class="info">
 					<view class="info-wrap">
-						<view class="nickname">admin01</view>
-						<view class="username">账号:admin01</view>
+						<view class="nickname" v-if="logins">admin01</view>
+						<view class="username" v-if="logins">账号:admin01</view>
+						<navigator url="../login/login"  v-else>
+							<view class="nickname">
+							登录/注册
+							</view>
+						</navigator>
 					</view>
 				</view>
 				<view>
-					<image src="../../static/imgs/icons/settings.png" class="icon-setting"></image>
+					<image src="../../static/imgs/icons/settings.png" class="icon-setting" v-if="logins"></image>
 				</view>
 			</view>
 		</view>
@@ -24,7 +30,7 @@
 	export default {
 		data() {
 			return {
-				
+				logins: false
 			}
 		},
 		methods: {
