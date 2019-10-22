a6c0166dbf6a4af74ce245d03d8887309b973032
juerui <juerui@vip.qq.com>
2019-10-22 19:18:08 +0800

    头像上传编辑页-新建页面

---
+++ pages/my/userInfo/avatarUpload/avatarUpload.vue
@@ -0,0 +1,20 @@
+<template>
+	<!-- 头像上传 -->
+	<view>
+		<!-- <image src="../../../../static/imgs/icons/error.png" mode=""></image> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import './avatarUpload.scss'
+</style>
