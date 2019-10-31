e4ecd83f343f44bccb9cec36577074f993cf95bf
juerui <juerui@vip.qq.com>
2019-10-31 11:15:52 +0800

    api接口--本地获取

--- common/api.js
+++ common/api.js
@@ -1,6 +1,8 @@
 const serverUrl = "https://www.imovietrailer.com/superhero";			// 生产环境
 // 权限QQ
-const qq = 'lee31526511';
+// const qq = 'lee31526511';
+const qq = uni.getStorageSync('qq') || '';
+
 // const serverUrl = "https://www.imovietrailer-dev.com/superhero";		// 开发环境
 export default{	
 	serverUrl,
